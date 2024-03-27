748b8ef2cb8c2c109cb9282eadea9da9f22b6edd
nhajthofa8 <huynhnhathoa1192001@gmail.com>
2024-03-27 10:27:01 +0700

    tesst

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../app/controllers/UserControllers')
-const { authMiddleWare, authUserMiddleWare } = require("../middleware/authMiddleware");
+const { authMiddleWare, authUserMiddleWare } = require("../middlewares/authMiddleware");
 router.post('/sign-up', userController.createUser)
 router.post('/sign-in', userController.loginUser)
 router.put('/update-user/:id', authUserMiddleWare, userController.updateUser)
